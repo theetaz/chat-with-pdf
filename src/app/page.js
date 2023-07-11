bd7965160bdd94213673db5ab8db5a2b1d29771c
Kusal Kalinga <kusalkalingainfo@gmail.com>
2023-07-11 19:37:40 +0530

    metadata added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
 import styles from "./page.module.css";
 import HeroPage from "@/view/HeroPage";
 
+export const metadata = {
+  title: "Chat with PDF",
+  description: "Let's chat with PDF",
+};
 
 export default function Home() {
   return (
