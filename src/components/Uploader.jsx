346bfa06766178280bf0c4dd5caf4b93bed666fb
Kusal Kalinga <kusalkalingainfo@gmail.com>
2023-07-11 16:04:56 +0530

    routing

--- src/components/Uploader.jsx
+++ src/components/Uploader.jsx
@@ -1,34 +1,51 @@
 "use client";
 
-
 import { message, Upload } from "antd";
+import { useRouter } from "next/navigation";
 const { Dragger } = Upload;
 
-const props = {
-  name: "file",
-  multiple: false,
-  action: "",
-  accept: ".pdf",
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== "uploading") {
-      console.log(info.file, info.fileList);
+import React, { use, useEffect, useState } from "react";
+
+const Uploader = () => {
+  const router = useRouter();
+  const [uploadedFile, setUploadedFile] = useState(null);
+
+  const props = {
+    name: "file",
+    multiple: false,
+    action: "http://localhost:8000/api/v1/chatdoc/",
+    accept: ".pdf",
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (status === "done") {
+        message.success(`${info.file.name} file uploaded successfully.`);
+        console.log(info.file.response.result);
+        setUploadedFile(info.file.response.result);
+      } else if (status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+    onDrop(e) {
+      console.log("Dropped files", e.dataTransfer.files);
+    },
+  };
+
+  useEffect(() => {
+    //route to chat page id
+    if (uploadedFile) {
+      router.push(`/chat/${uploadedFile.id}`);
     }
-    if (status === "done") {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === "error") {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log("Dropped files", e.dataTransfer.files);
-  },
+  }, [uploadedFile]);
+
+  return (
+    <Dragger {...props} className="uploader">
+      <p className="ant-upload-drag-icon">+ New Chat</p>
+      <p className="ant-upload-text">Click or drag PDF file</p>
+    </Dragger>
+  );
 };
 
-const Uploader = () => (
-  <Dragger {...props} className="uploader">
-    <p className="ant-upload-drag-icon">+ New Chat</p>
-    <p className="ant-upload-text">Click or drag PDF file</p>
-  </Dragger>
-);
 export default Uploader;
