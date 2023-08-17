47e7c8bd8cae342bb9008ee8b593a929da56e25c
Kusal Kalinga <kusalkalingainfo@gmail.com>
2023-08-17 23:06:11 +0530

    ui chnages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,56 +4,75 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <Link
-        href="/about"
-        style={{
-          marginRight: "10px",
-          textDecoration: "none",
+      <div className="d-flex flex-column align-items-center justify-content-center">
+        <div>
+          <Link
+            href="/about"
+            style={{
+              marginRight: "35px",
+              textDecoration: "none",
+              color: "#8496aa",
+            }}
+          >
+            About
+          </Link>
+          <Link
+            href="/about"
+            style={{
+              marginRight: "35px",
+              textDecoration: "none",
+              color: "#8496aa",
+            }}
+          >
+            Pricing
+          </Link>
+          <Link
+            href="/about"
+            style={{
+              marginRight: "35px",
+              textDecoration: "none",
+              color: "#8496aa",
+            }}
+          >
+            FAQ
+          </Link>
+          <Link
+            href="/about"
+            style={{
+              marginRight: "35px",
+              textDecoration: "none",
+              color: "#8496aa",
+            }}
+          >
+            Affiliate
+          </Link>
+          <Link
+            href="/about"
+            style={{
+              marginRight: "25px",
+              textDecoration: "none",
+              color: "#8496aa",
+            }}
+          >
+            Twitter
+          </Link>
+        </div>
+        <div style={{
           color: "#8496aa",
-        }}
-      >
-        About
-      </Link>
-      <Link
-        href="/about"
-        style={{
-          marginRight: "10px",
-          textDecoration: "none",
-          color: "#8496aa",
-        }}
-      >
-        Pricing
-      </Link>
-      <Link
-        href="/about"
-        style={{
-          marginRight: "10px",
-          textDecoration: "none",
-          color: "#8496aa",
-        }}
-      >
-        FAQ
-      </Link>
-      <Link
-        href="/about"
-        style={{
-          marginRight: "10px",
-          textDecoration: "none",
-          color: "#8496aa",
-        }}
-      >
-        Affiliate
-      </Link>
-      <Link
-        href="/about"
-        style={{
-          marginRight: "10px",
-          textDecoration: "none",
-          color: "#8496aa",
-        }}
-      >
-        Twitter
-      </Link>
+          fontSize: "14px",
+        }}> 
+          Copyright chat-with-pdf 2023, All Rights Reserved. Powered{" "}by{" "}
+          <Link
+            href="/"
+            style={{
+              textDecoration: "none",
+              
+            }}
+          >
+            Perfectus
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
