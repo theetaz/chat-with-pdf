620eeda94d75ef264cbb2360e344a98df7022713
Kusal Kalinga <kusalkalingainfo@gmail.com>
2023-09-18 11:58:42 +0530

    meta data added

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,12 @@ import { Provider } from "react-redux";
 import store from "@/redux/store";
 import AuthProvider from "./context/AuthContext";
 
+// export const metadata = {
+//   title: "Smart AI Doc",
+//   description:
+//     "Your smart AI document reader & analyzer - similar to ChatGPT but for PDFs, CSVs, Excel, and PowerPoint documents. Summarize and respond to questions intelligently",
+//   metadataBase: new URL(process.env.VERCEL_URL),
+// };
 
 export default function RootLayout({ children }) {
   return (
@@ -16,7 +22,6 @@ export default function RootLayout({ children }) {
       </Head>
       <body>
         <Provider store={store}>
-          
           <AuthProvider>{children}</AuthProvider>
         </Provider>
       </body>
